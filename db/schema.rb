e0bdad98e0c0f9549d696b13d84eef6f58cf275a
Maciej Sypniewski <msypniewski511@gmail.com>
2017-03-26 09:53:47 +0200

    Cleared database

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,6 @@ ActiveRecord::Schema.define(version: 20170323003203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_products", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "admin_publishers", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
