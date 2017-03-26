a4d5aa5ec546efac1bd14014f3e4a0269ee56644
Maciej Sypniewski <msypniewski511@gmail.com>
2017-03-26 09:52:29 +0200

    Reset database

--- db/migrate/20170321191029_create_admin_publishers.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdminPublishers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :admin_publishers do |t|
-      t.string :name, :limit => 255, :null => false, :unique => true
-      t.timestamps
-    end
-  end
-end
